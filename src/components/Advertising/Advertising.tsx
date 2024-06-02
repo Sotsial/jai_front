21b9f1b3e23155c3e8c803bebd11e02c4c8c2bd4
Саян Социал <s.social@arta.pro>
2024-06-02 16:41:47 +0500

    addsence

--- src/components/Advertising/Advertising.tsx
+++ src/components/Advertising/Advertising.tsx
@@ -1,26 +1,36 @@
-import { Card, Space, Typography } from "antd";
 import "./Advertising.css";
-import carImage from "src/assets/car.png";
 import { isMobile } from "react-device-detect";
+import { Adsense } from "@ctrl/react-adsense";
 
 const Advertising = () => {
   if (isMobile)
     return (
-      <Card
-        style={{ minHeight: "200px", textAlign: "center", borderRadius: 0 }}
-      >
-        <Typography.Title level={2}>Рекламный баннер</Typography.Title>
-      </Card>
+      // <Card
+      //   style={{ minHeight: "200px", textAlign: "center", borderRadius: 0 }}
+      // >
+      //   <Typography.Title level={2}>Рекламный баннер</Typography.Title>
+      // </Card>
+      <Adsense
+        client="ca-pub-6168437561514335"
+        slot="6338645549"
+        format="auto"
+      />
     );
   return (
-    <div className="advertising">
-      <Space size={24}>
-        <img src={carImage} className="car_image" />
-        <Typography.Title className="advertising_text">
-          Рекламный баннер
-        </Typography.Title>
-      </Space>
-    </div>
+    // <div className="advertising">
+    //   <Space size={24}>
+    //     <img src={carImage} className="car_image" />
+    //     <Typography.Title className="advertising_text">
+    //       Рекламный баннер
+    //     </Typography.Title>
+    //   </Space>
+    // </div>
+    <Adsense
+      client="ca-pub-6168437561514335"
+      slot="6338645549"
+      format="auto"
+      layout=""
+    />
   );
 };
 
